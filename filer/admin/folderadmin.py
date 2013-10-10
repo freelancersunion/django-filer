1ec80e4c8aeb551597ac2dcac66741a0bca8d691
sbussetti <sbussetti@freelancersunion.org>
2013-10-10 10:11:48 -0400

    for now i just need files and folders sorted by most recent uploaded/modified first.. next need to dig in and refactor the whole thing to use ChangeLists so we can properly sort and filter

--- filer/admin/folderadmin.py
+++ filer/admin/folderadmin.py
@@ -279,8 +279,8 @@ class FolderAdmin(PrimitivePermissionAwareModelAdmin):
             file_qs = folder.files.all()
             show_result_count = False
 
-        folder_qs = folder_qs.order_by('name')
-        file_qs = file_qs.order_by('name')
+        folder_qs = folder_qs.order_by('-modified_at')
+        file_qs = file_qs.order_by('-uploaded_at')
 
         folder_children = []
         folder_files = []
@@ -304,8 +304,9 @@ class FolderAdmin(PrimitivePermissionAwareModelAdmin):
             }
         except:
             permissions = {}
-        folder_files.sort()
+        #folder_files.sort()
         items = folder_children + folder_files
+
         paginator = Paginator(items, FILER_PAGINATE_BY)
 
         # Are we moving to clipboard?
